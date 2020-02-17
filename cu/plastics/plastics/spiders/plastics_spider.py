db0ab188914ee18c34d688c9e3a36d1ddffc2d8d
pc-d <60708506+pc-d@users.noreply.github.com>
2020-02-16 22:23:57 -0500

    Add files via upload

--- cu/plastics/plastics/spiders/plastics_spider.py
+++ cu/plastics/plastics/spiders/plastics_spider.py
@@ -1,17 +1,36 @@
 import scrapy
+from plastics import items
 
 class ApprovedPlastics(scrapy.Spider):
     name = "plastics"
     
-    def start_request(self):
-        start_irls = [
-            "https://newsforkids.net/fastfacts/microplastics/",
+    def start_requests(self):
         
-        ]
+        urls = [
+            "https://www.acplasticsinc.com/informationcenter/r/fda-approved-plastics-for-food-contact"
+            ]
+        
+        for url in urls:
+            yield scrapy.Request(url=url,callback=self.parse)
+
 
             
     def parse(self,response):
+
         filename = 'ApprovedPlastics'
         with open(filename,'wb') as f:
             f.write(response.body)
+        self.log('Saved file %s' % filename)
+                
+        item = items()
+        iemt['main_headline']=response.xpath('//span/text()').extract()
+        item['headline']=response.xpath('//title/text()').extract()
+        item['url']=response.url
+        item['project']=self.settings.get('BOT_NAME')
+        item['spider']=self.name
+
+        return item
+         
+        
+
     print('Saved file')
